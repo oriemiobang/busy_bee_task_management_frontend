cc57b36eebf8ba21e78a3c6caa865aacdcf55d17
oriemiobang <oriemiobango@gmail.com>
2026-02-26 21:21:47 +0300

    code cleaning

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:frontend/app.dart';
+// import 'package:frontend/app.dart';
 
 import 'package:frontend/main.dart';
 
