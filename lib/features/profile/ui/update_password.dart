a3b6e846021e9a136430917a35b5a9b666959d7b
oriemiobang <oriemiobango@gmail.com>
2026-02-26 20:35:16 +0300

    code cleaning

--- lib/features/profile/ui/update_password.dart
+++ lib/features/profile/ui/update_password.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/features/auth/state/auth_provider.dart';
-import 'package:frontend/routes/app_routes.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
